c1a29729b5ea6cee9f500338a9fab66a9ff03625
Recolic Keghart <root@recolic.net>
2020-11-15 20:43:41 +0800

    u

--- include/aconfig.h
+++ include/aconfig.h
@@ -1,128 +1,129 @@
+/* include/aconfig.h.  Generated from aconfig.h.in by configure.  */
 /* include/aconfig.h.in.  Generated from configure.in by autoheader.  */
 
 /* directory containing alsa configuration */
-#undef DATADIR
+#define DATADIR "/usr/share/alsa"
 
 /* Define to 1 if translation of program messages to the user's native
    language is requested. */
-#undef ENABLE_NLS
+#define ENABLE_NLS 1
 
 /* Define if curses-based programs can show translated messages. */
-#undef ENABLE_NLS_IN_CURSES
+#define ENABLE_NLS_IN_CURSES 1
 
 /* Define to 1 if you have the <alsa/mixer.h> header file. */
-#undef HAVE_ALSA_MIXER_H
+#define HAVE_ALSA_MIXER_H 1
 
 /* Define to 1 if you have the <alsa/pcm.h> header file. */
-#undef HAVE_ALSA_PCM_H
+#define HAVE_ALSA_PCM_H 1
 
 /* Define to 1 if you have the <alsa/rawmidi.h> header file. */
-#undef HAVE_ALSA_RAWMIDI_H
+#define HAVE_ALSA_RAWMIDI_H 1
 
 /* Define to 1 if you have the <alsa/seq.h> header file. */
-#undef HAVE_ALSA_SEQ_H
+#define HAVE_ALSA_SEQ_H 1
 
 /* Define to 1 if you have the MacOS X function CFLocaleCopyCurrent in the
    CoreFoundation framework. */
-#undef HAVE_CFLOCALECOPYCURRENT
+/* #undef HAVE_CFLOCALECOPYCURRENT */
 
 /* Define to 1 if you have the MacOS X function CFPreferencesCopyAppValue in
    the CoreFoundation framework. */
-#undef HAVE_CFPREFERENCESCOPYAPPVALUE
+/* #undef HAVE_CFPREFERENCESCOPYAPPVALUE */
 
 /* Have clock gettime */
-#undef HAVE_CLOCK_GETTIME
+#define HAVE_CLOCK_GETTIME 1
 
 /* Have curses set_escdelay */
-#undef HAVE_CURSES_ESCDELAY
+#define HAVE_CURSES_ESCDELAY 1
 
 /* Define if the GNU dcgettext() function is already present or preinstalled.
    */
-#undef HAVE_DCGETTEXT
+#define HAVE_DCGETTEXT 1
 
 /* Define to 1 if you have the <form.h> header file. */
-#undef HAVE_FORM_H
+#define HAVE_FORM_H 1
 
 /* Define if the GNU gettext() function is already present or preinstalled. */
-#undef HAVE_GETTEXT
+#define HAVE_GETTEXT 1
 
 /* Define if you have the iconv() function. */
-#undef HAVE_ICONV
+/* #undef HAVE_ICONV */
 
 /* Define to 1 if you have the <inttypes.h> header file. */
-#undef HAVE_INTTYPES_H
+#define HAVE_INTTYPES_H 1
 
 /* Define to 1 if you have the `asound' library (-lasound). */
-#undef HAVE_LIBASOUND
+#define HAVE_LIBASOUND 1
 
 /* Have librt */
-#undef HAVE_LIBRT
+#define HAVE_LIBRT 1
 
 /* Define to 1 if you have the <memory.h> header file. */
-#undef HAVE_MEMORY_H
+#define HAVE_MEMORY_H 1
 
 /* Define to 1 if you have the <menu.h> header file. */
-#undef HAVE_MENU_H
+#define HAVE_MENU_H 1
 
 /* Define to 1 if you have the <panel.h> header file. */
-#undef HAVE_PANEL_H
+#define HAVE_PANEL_H 1
 
 /* Define to 1 if you have the <samplerate.h> header file. */
-#undef HAVE_SAMPLERATE_H
+#define HAVE_SAMPLERATE_H 1
 
 /* Define to 1 if you have the <stdint.h> header file. */
-#undef HAVE_STDINT_H
+#define HAVE_STDINT_H 1
 
 /* Define to 1 if you have the <stdlib.h> header file. */
-#undef HAVE_STDLIB_H
+#define HAVE_STDLIB_H 1
 
 /* Define to 1 if you have the <strings.h> header file. */
-#undef HAVE_STRINGS_H
+#define HAVE_STRINGS_H 1
 
 /* Define to 1 if you have the <string.h> header file. */
-#undef HAVE_STRING_H
+#define HAVE_STRING_H 1
 
 /* Define to 1 if you have the <sys/stat.h> header file. */
-#undef HAVE_SYS_STAT_H
+#define HAVE_SYS_STAT_H 1
 
 /* Define to 1 if you have the <sys/types.h> header file. */
-#undef HAVE_SYS_TYPES_H
+#define HAVE_SYS_TYPES_H 1
 
 /* Define to 1 if you have the <unistd.h> header file. */
-#undef HAVE_UNISTD_H
+#define HAVE_UNISTD_H 1
 
 /* Name of package */
-#undef PACKAGE
+#define PACKAGE "alsa-utils"
 
 /* Define to the address where bug reports for this package should be sent. */
-#undef PACKAGE_BUGREPORT
+#define PACKAGE_BUGREPORT ""
 
 /* Define to the full name of this package. */
-#undef PACKAGE_NAME
+#define PACKAGE_NAME ""
 
 /* Define to the full name and version of this package. */
-#undef PACKAGE_STRING
+#define PACKAGE_STRING ""
 
 /* Define to the one symbol short name of this package. */
-#undef PACKAGE_TARNAME
+#define PACKAGE_TARNAME ""
 
 /* Define to the home page for this package. */
-#undef PACKAGE_URL
+#define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#undef PACKAGE_VERSION
+#define PACKAGE_VERSION ""
 
 /* directory containing sample data */
-#undef SOUNDSDIR
+#define SOUNDSDIR "/usr/share/sounds/alsa"
 
 /* Define to 1 if you have the ANSI C header files. */
-#undef STDC_HEADERS
+#define STDC_HEADERS 1
 
 /* Define to 1 if you can safely include both <sys/time.h> and <time.h>. */
-#undef TIME_WITH_SYS_TIME
+#define TIME_WITH_SYS_TIME 1
 
 /* ALSA util version */
-#undef VERSION
+#define VERSION "1.0.24.2"
 
 /* Enable large inode numbers on Mac OS X 10.5.  */
 #ifndef _DARWIN_USE_64_BIT_INODE
@@ -130,16 +131,16 @@
 #endif
 
 /* Number of bits in a file offset, on hosts where this is settable. */
-#undef _FILE_OFFSET_BITS
+/* #undef _FILE_OFFSET_BITS */
 
 /* Define for large files, on AIX-style hosts. */
-#undef _LARGE_FILES
+/* #undef _LARGE_FILES */
 
 /* Define to empty if `const' does not conform to ANSI C. */
-#undef const
+/* #undef const */
 
 /* Define to `__inline__' or `__inline' if that's what the C compiler
    calls it, or to nothing if 'inline' is not supported under any name.  */
 #ifndef __cplusplus
-#undef inline
+/* #undef inline */
 #endif
